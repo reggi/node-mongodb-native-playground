a481e0e134d2de1202f8f080faa29afbf8462e70
Thomas Reggi <thomas.reggi@mongodb.com>
2020-09-14 10:00:10 -0400

    isolated

--- NODE-2784/index.js
+++ NODE-2784/index.js
@@ -14,7 +14,7 @@ const reporterOptions = {
 
 describe(`NODE-2784 - ${MONGODB_URI}`, () => {
 
-  it.skip('should simulate reporter scenario', async () => {
+  it('should simulate reporter scenario', async () => {
     const client = new MongoClient(MONGODB_URI, {
       useUnifiedTopology: true,
       ...reporterOptions
@@ -24,44 +24,36 @@ describe(`NODE-2784 - ${MONGODB_URI}`, () => {
     const collection = await db.collection('collection-one')
     // insert data
     const insertManyResult = await collection.insertMany(documents);
-    assert.equal(insertManyResult.result.ok, 1); // check to see if writes were successful
+    assert.strictEqual(insertManyResult.result.ok, 1); // check to see if writes were successful
     // create initial index
     const createdIndexA = await collection.createIndex({ quote : "text" });
-    assert.equal(createdIndexA, 'quote_text');
+    assert.strictEqual(createdIndexA, 'quote_text');
     // try and create same index again to simulate it exists
     const createdIndexB = await collection.createIndex({ quote : "text" });
-    assert.equal(createdIndexB, 'quote_text');
+    assert.strictEqual(createdIndexB, 'quote_text');
     // check the indexes
     const listIndexResult = await collection.listIndexes().toArray()
-    assert.equal(listIndexResult[0].name, '_id_'); // default index
-    assert.equal(listIndexResult[1].name, 'quote_text'); // created index
-    assert.equal(listIndexResult.length, 2);
+    assert.strictEqual(listIndexResult[0].name, '_id_'); // default index
+    assert.strictEqual(listIndexResult[1].name, 'quote_text'); // created index
+    assert.strictEqual(listIndexResult.length, 2);
     // ensure collection is still there
     const find = await collection.find().toArray();
-    assert.equal(find.length, 3); // check to see if the original items are still there
+    assert.strictEqual(find.length, 3); // check to see if the original items are still there
     await client.close(true)
   })
 
-  it("should create index on collection that doesn't exist", async () => {
-    console.log('a');
+  const _it = (process.env.ISOLATE) ? it.only : it;
+
+  _it("should create index on collection that doesn't exist [async]", async () => {
     const client = new MongoClient(MONGODB_URI, {
       useUnifiedTopology: true,
       ...reporterOptions
     })
-    console.log('b');
     await client.connect()
-    console.log('c');
     const db = client.db('db-two')
-    console.log('d');
     const collection = await db.collection('db-two')
-    console.log('e');
-    console.log('f');
     const createdIndexA = await collection.createIndex({ quote : "text" });
-    console.log('g');
-    assert.equal(createdIndexA, 'quote_text');
-    console.log('h');
+    assert.strictEqual(createdIndexA, 'quote_text');
     await client.close(true)
   });
-
 });
-
